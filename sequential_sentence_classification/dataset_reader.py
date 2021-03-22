86fb753872fff1540a25100f4b453ed701d52205
UrszulaCzerwinska <ulcia.liberte@gmail.com>
2021-03-22 14:29:43 +0000

    deleted parts dedicated to sep

--- sequential_sentence_classification/dataset_reader.py
+++ sequential_sentence_classification/dataset_reader.py
@@ -35,10 +35,6 @@ class SeqClassificationReader(DatasetReader):
                  tokenizer: Tokenizer = None,
                  sent_max_len: int = 100,
                  max_sent_per_example: int = 20,
-                 use_sep: bool = True,
-                 sci_sum: bool = False,
-                 use_abstract_scores: bool = True,
-                 sci_sum_fake_scores: bool = True,
                  predict: bool = False,
                  ) -> None:
         super().__init__(manual_distributed_sharding=True,
@@ -46,19 +42,13 @@ class SeqClassificationReader(DatasetReader):
         self._tokenizer = tokenizer or WhitespaceTokenizer()
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
         self.sent_max_len = sent_max_len
-        self.use_sep = use_sep
         self.predict = predict
-        self.sci_sum = sci_sum
         self.max_sent_per_example = max_sent_per_example
-        self.use_abstract_scores = use_abstract_scores
-        self.sci_sum_fake_scores = sci_sum_fake_scores
         print("*********************************")
         print("start token : ", self._tokenizer.sequence_pair_start_tokens)
         print("middle token : ", self._tokenizer.sequence_pair_mid_tokens)
         print("end token : ", self._tokenizer.sequence_pair_end_tokens)
         print("*********************************")
-
-
 
     def _read(self, file_path: str):
         file_path = cached_path(file_path)
@@ -82,31 +72,10 @@ class SeqClassificationReader(DatasetReader):
         confidences = json_dict.get("confs", None)
 
         additional_features = None
-        if self.sci_sum:
-            if self.sci_sum_fake_scores:
-                labels = [np.random.rand() for _ in sentences]
-            else: 
-                labels = [s if s > 0 else 0.000001 for s in json_dict["highlight_scores"]]
-
-            if self.use_abstract_scores:
-                features = []
-                if self.use_abstract_scores:
-                    if self.sci_sum_fake_scores:
-                        abstract_scores = [np.random.rand() for _ in sentences]
-                    else:
-                        abstract_scores = json_dict["abstract_scores"]
-                    features.append(abstract_scores)
-                    
-                additional_features = list(map(list, zip(*features)))  # some magic transpose function
-
-            sentences, labels = self.filter_bad_sci_sum_sentences(sentences, labels)
-
-            if len(sentences) == 0:
-                return []
-
-        for sentences_loop, labels_loop, confidences_loop, additional_features_loop in  \
-                self.enforce_max_sent_per_example(sentences, labels, confidences, additional_features):
-
+        
+        # for sentences_loop, labels_loop, confidences_loop, additional_features_loop in  \
+        #         self.enforce_max_sent_per_example(sentences, labels, confidences, additional_features):
+        for sentences_loop, labels_loop, confidences_loop, additional_features_loop in (sentences, labels, confidences, additional_features):
             instance = self.text_to_instance(
                 sentences=sentences_loop,
                 labels=labels_loop,
@@ -116,67 +85,67 @@ class SeqClassificationReader(DatasetReader):
             instances.append(instance)
         return instances
 
-    def enforce_max_sent_per_example(self, sentences, labels=None, confidences=None, additional_features=None):
-        """
-        Splits examples with len(sentences) > self.max_sent_per_example into multiple smaller examples
-        with len(sentences) <= self.max_sent_per_example.
-        Recursively split the list of sentences into two halves until each half
-        has len(sentences) < <= self.max_sent_per_example. The goal is to produce splits that are of almost
-        equal size to avoid the scenario where all splits are of size
-        self.max_sent_per_example then the last split is 1 or 2 sentences
-        This will result into losing context around the edges of each examples.
-        """
-        if labels is not None:
-            assert len(sentences) == len(labels)
-        if confidences is not None:
-            assert len(sentences) == len(confidences)
-        if additional_features is not None:
-            assert len(sentences) == len(additional_features)
+    # def enforce_max_sent_per_example(self, sentences, labels=None, confidences=None, additional_features=None):
+    #     """
+    #     Splits examples with len(sentences) > self.max_sent_per_example into multiple smaller examples
+    #     with len(sentences) <= self.max_sent_per_example.
+    #     Recursively split the list of sentences into two halves until each half
+    #     has len(sentences) < <= self.max_sent_per_example. The goal is to produce splits that are of almost
+    #     equal size to avoid the scenario where all splits are of size
+    #     self.max_sent_per_example then the last split is 1 or 2 sentences
+    #     This will result into losing context around the edges of each examples.
+    #     """
+    #     if labels is not None:
+    #         assert len(sentences) == len(labels)
+    #     if confidences is not None:
+    #         assert len(sentences) == len(confidences)
+    #     if additional_features is not None:
+    #         assert len(sentences) == len(additional_features)
 
-        if len(sentences) > self.max_sent_per_example and self.max_sent_per_example > 0:
-            i = len(sentences) // 2
-            l1 = self.enforce_max_sent_per_example(
-                    sentences[:i], None if labels is None else labels[:i],
-                    None if confidences is None else confidences[:i],
-                    None if additional_features is None else additional_features[:i])
-            l2 = self.enforce_max_sent_per_example(
-                    sentences[i:], None if labels is None else labels[i:],
-                    None if confidences is None else confidences[i:],
-                    None if additional_features is None else additional_features[i:])
-            return l1 + l2
-        else:
-            return [(sentences, labels, confidences, additional_features)]
+    #     if len(sentences) > self.max_sent_per_example and self.max_sent_per_example > 0:
+    #         i = len(sentences) // 2
+    #         l1 = self.enforce_max_sent_per_example(
+    #                 sentences[:i], None if labels is None else labels[:i],
+    #                 None if confidences is None else confidences[:i],
+    #                 None if additional_features is None else additional_features[:i])
+    #         l2 = self.enforce_max_sent_per_example(
+    #                 sentences[i:], None if labels is None else labels[i:],
+    #                 None if confidences is None else confidences[i:],
+    #                 None if additional_features is None else additional_features[i:])
+    #         return l1 + l2
+    #     else:
+    #         return [(sentences, labels, confidences, additional_features)]
 
-    def is_bad_sentence(self, sentence: str):
-        if len(sentence) > 10 and len(sentence) < 600:
-            return False
-        else:
-            return True
+    # def is_bad_sentence(self, sentence: str):
+    #     if len(sentence) > 10 and len(sentence) < 600:
+    #         return False
+    #     else:
+    #         return True
 
-    def filter_bad_sci_sum_sentences(self, sentences, labels):
-        filtered_sentences = []
-        filtered_labels = []
-        if not self.predict:
-            for sentence, label in zip(sentences, labels):
-                # most sentences outside of this range are bad sentences
-                if not self.is_bad_sentence(sentence):
-                    filtered_sentences.append(sentence)
-                    filtered_labels.append(label)
-                else:
-                    filtered_sentences.append("BADSENTENCE")
-                    filtered_labels.append(0.000001)
-            sentences = filtered_sentences
-            labels = filtered_labels
-        else:
-            for sentence in sentences:
-                # most sentences outside of this range are bad sentences
-                if not self.is_bad_sentence(sentence):
-                    filtered_sentences.append(sentence)
-                else:
-                    filtered_sentences.append("BADSENTENCE")
-            sentences = filtered_sentences
+    # def filter_bad_sci_sum_sentences(self, sentences, labels):
+    #     filtered_sentences = []
+    #     filtered_labels = []
+    #     if not self.predict:
+    #         for sentence, label in zip(sentences, labels):
+    #             # most sentences outside of this range are bad sentences
+    #             if not self.is_bad_sentence(sentence):
+    #                 filtered_sentences.append(sentence)
+    #                 filtered_labels.append(label)
+    #             else:
+    #                 filtered_sentences.append("BADSENTENCE")
+    #                 filtered_labels.append(0.000001)
+    #         sentences = filtered_sentences
+    #         labels = filtered_labels
+    #     else:
+    #         for sentence in sentences:
+    #             # most sentences outside of this range are bad sentences
+    #             if not self.is_bad_sentence(sentence):
+    #                 filtered_sentences.append(sentence)
+    #             else:
+    #                 filtered_sentences.append("BADSENTENCE")
+    #         sentences = filtered_sentences
 
-        return sentences, labels
+    #     return sentences, labels
 
     def text_to_instance(self,
                          sentences: List[str],
@@ -191,28 +160,15 @@ class SeqClassificationReader(DatasetReader):
         if additional_features is not None:
             assert len(sentences) == len(additional_features)
 
-        if self.use_sep:
-            origin_sent = copy.deepcopy(sentences)
-            sentences = self.shorten_sentences(sentences, self.sent_max_len)
-    
-            max_len=self.sent_max_len
-            while len(sentences[0]) > 512:
-                n = int((len(sentences[0])-512)/ len(origin_sent))+1
-                
-                max_len -= n
-                sentences = self.shorten_sentences(origin_sent, max_len )
-              
-            assert len(sentences[0]) <= 512
-    
-        else:
-            tok_sentences = []
-            for sentence_text in sentences:
-                if len(self._tokenizer.tokenize(sentence_text)) > self.sent_max_len:
-                    tok_sentences.append(self._tokenizer.tokenize(sentence_text)[:self.sent_max_len]+self._tokenizer.sequence_pair_end_tokens)
-                else:
-                    tok_sentences.append(self._tokenizer.tokenize(sentence_text))
-            
-            sentences = tok_sentences
+     
+        tok_sentences = []
+        for sentence_text in sentences:
+            if len(self._tokenizer.tokenize(sentence_text)) > self.sent_max_len:
+                tok_sentences.append(self._tokenizer.tokenize(sentence_text)[:self.sent_max_len]+self._tokenizer.sequence_pair_end_tokens)
+            else:
+                tok_sentences.append(self._tokenizer.tokenize(sentence_text))
+        
+        sentences = tok_sentences
            
         fields: Dict[str, Field] = {}
         fields["sentences"] = ListField([
@@ -228,12 +184,10 @@ class SeqClassificationReader(DatasetReader):
             else:
                 # make the labels strings for easier identification of the neutral label
                 # probably not strictly necessary
-                if self.sci_sum:
-                    fields["labels"] = ArrayField(np.array(labels))
-                else:
-                    fields["labels"] = ListField([
-                            LabelField(str(label)+"_label") for label in labels
-                        ])
+
+                fields["labels"] = ListField([
+                        LabelField(str(label)+"_label") for label in labels
+                    ])
 
         if confidences is not None:
             fields['confidences'] = ArrayField(np.array(confidences))
@@ -246,12 +200,12 @@ class SeqClassificationReader(DatasetReader):
         for text_field in instance["sentences"].field_list:
             text_field.token_indexers = self._token_indexers
 
-    def shorten_sentences(self, origin_sent, max_len):
-        tokenized_sentences = [self._tokenizer.sequence_pair_start_tokens]
-        for s in origin_sent:
-            if len(self._tokenizer.tokenize(s)) > (max_len):
-                tokenized_sentences.append(self._tokenizer.tokenize(s)[1:(max_len)]+self._tokenizer.sequence_pair_mid_tokens)
-            else:
-                tokenized_sentences.append(self._tokenizer.tokenize(s)[1:-1]+self._tokenizer.sequence_pair_mid_tokens)
-        mid_tok_len = len(self._tokenizer.sequence_pair_mid_tokens)
-        return [list(itertools.chain.from_iterable(tokenized_sentences))[:-mid_tok_len]+self._tokenizer.sequence_pair_end_tokens]
+    # def shorten_sentences(self, origin_sent, max_len):
+    #     tokenized_sentences = [self._tokenizer.sequence_pair_start_tokens]
+    #     for s in origin_sent:
+    #         if len(self._tokenizer.tokenize(s)) > (max_len):
+    #             tokenized_sentences.append(self._tokenizer.tokenize(s)[1:(max_len)]+self._tokenizer.sequence_pair_mid_tokens)
+    #         else:
+    #             tokenized_sentences.append(self._tokenizer.tokenize(s)[1:-1]+self._tokenizer.sequence_pair_mid_tokens)
+    #     mid_tok_len = len(self._tokenizer.sequence_pair_mid_tokens)
+    #     return [list(itertools.chain.from_iterable(tokenized_sentences))[:-mid_tok_len]+self._tokenizer.sequence_pair_end_tokens]
--- sequential_sentence_classification/model.py
+++ sequential_sentence_classification/model.py
@@ -21,49 +21,34 @@ class SeqClassificationModel(Model):
 
     def __init__(self, vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
-                 use_sep: bool = True,
                  with_crf: bool = False,
                  self_attn: Seq2SeqEncoder = None,
-                 bert_dropout: float = 0.1,
-                 sci_sum: bool = False,
-                 intersentence_token: str = "[SEP]",
-                 model_type: str = "bert",
                  additional_feature_size: int = 0,
                  ) -> None:
         super(SeqClassificationModel, self).__init__(vocab)
 
         self.text_field_embedder = text_field_embedder
         self.vocab = vocab
-        self.use_sep = use_sep
         self.with_crf = with_crf
-        self.sci_sum = sci_sum
         self.self_attn = self_attn
         self.additional_feature_size = additional_feature_size
-        self.token = intersentence_token
-        self.model_type = model_type
         self.dropout = torch.nn.Dropout(p=bert_dropout)
 
-       # define loss
-        if self.sci_sum:
-            self.loss = torch.nn.MSELoss(reduction='none')  # labels are rouge scores
-            self.labels_are_scores = True
-            self.num_labels = 1
-        else:
-            self.loss = torch.nn.CrossEntropyLoss(ignore_index=-1, reduction='none')
-            self.labels_are_scores = False
-            self.num_labels = self.vocab.get_vocab_size(namespace='labels')
-            # define accuracy metrics
-            self.label_accuracy = CategoricalAccuracy()
-            self.label_f1_metrics = {}
 
-            # define F1 metrics per label
-            for label_index in range(self.num_labels):
-                label_name = self.vocab.get_token_from_index(namespace='labels', index=label_index)
-                self.label_f1_metrics[label_name] = F1Measure(label_index)
+        self.loss = torch.nn.CrossEntropyLoss(ignore_index=-1, reduction='none')
+        self.num_labels = self.vocab.get_vocab_size(namespace='labels')
+        # define accuracy metrics
+        self.label_accuracy = CategoricalAccuracy()
+        self.label_f1_metrics = {}
+
+        # define F1 metrics per label
+        for label_index in range(self.num_labels):
+            label_name = self.vocab.get_token_from_index(namespace='labels', index=label_index)
+            self.label_f1_metrics[label_name] = F1Measure(label_index)
 
         encoded_senetence_dim = text_field_embedder._token_embedders['bert'].get_output_dim()
 
-        ff_in_dim = encoded_senetence_dim if self.use_sep else self_attn.get_output_dim()
+        ff_in_dim = self_attn.get_output_dim()
         ff_in_dim += self.additional_feature_size
 
         self.time_distributed_aggregate_feedforward = Linear(ff_in_dim, self.num_labels)
@@ -96,80 +81,12 @@ class SeqClassificationModel(Model):
         mask = get_text_field_mask(sentences, num_wrapping_dims=1).float()
         batch_size, num_sentences, _, _ = embedded_sentences.size()
 
-        if self.use_sep:
-            # The following code collects vectors of the SEP tokens from all the examples in the batch,
-            # and arrange them in one list. It does the same for the labels and confidences.
-            # TODO: replace 103 with '[SEP]'
-            index_sep = int(self.vocab.get_token_index(token=self.token, namespace = "tags"))
-            sentences_mask = sentences['bert']["token_ids"] == index_sep # mask for all the SEP tokens in the batch
-            embedded_sentences = embedded_sentences[sentences_mask]  # given batch_size x num_sentences_per_example x sent_len x vector_len
-                                                                        # returns num_sentences_per_batch x vector_len
-            ## roberta only WORKS ONLY IF BATCH SIZE == 1
-            if self.model_type == "roberta" :            
-                assert batch_size == 1, "set batch size to 1 for RoBERTa"                                               
-                indx = np.arange(embedded_sentences.shape[0])
-                device = "cuda" 
-                sel_idx = torch.from_numpy(indx[indx%2==0]).to(device)# select only scond intersentence marker
-                embedded_sentences = torch.index_select(embedded_sentences, 0, sel_idx)
-            
-            assert embedded_sentences.dim() == 2
-            num_sentences = embedded_sentences.shape[0]
-            # for the rest of the code in this model to work, think of the data we have as one example
-            # with so many sentences and a batch of size 1
-            batch_size = 1
-            embedded_sentences = embedded_sentences.unsqueeze(dim=0)
-            embedded_sentences = self.dropout(embedded_sentences)
-
-            if labels is not None:
-                if self.labels_are_scores:
-                    labels_mask = labels != 0.0  # mask for all the labels in the batch (no padding)
-                else:
-                    labels_mask = labels != -1  # mask for all the labels in the batch (no padding)
-
-                labels = labels[labels_mask]  # given batch_size x num_sentences_per_example return num_sentences_per_batch
-                assert labels.dim() == 1
-                if confidences is not None:
-                    confidences = confidences[labels_mask]
-                    assert confidences.dim() == 1
-                if additional_features is not None:
-                    additional_features = additional_features[labels_mask]
-                    assert additional_features.dim() == 2
-
-                num_labels = labels.shape[0]
-                if num_labels != num_sentences:  # bert truncates long sentences, so some of the SEP tokens might be gone
-                    assert num_labels > num_sentences  # but `num_labels` should be at least greater than `num_sentences`
-                    logger.warning(f'Found {num_labels} labels but {num_sentences} sentences')
-                    labels = labels[:num_sentences]  # Ignore some labels. This is ok for training but bad for testing.
-                                                        # We are ignoring this problem for now.
-                                                        # TODO: fix, at least for testing
-
-                # do the same for `confidences`
-                if confidences is not None:
-                    num_confidences = confidences.shape[0]
-                    if num_confidences != num_sentences:
-                        assert num_confidences > num_sentences
-                        confidences = confidences[:num_sentences]
-
-                # and for `additional_features`
-                if additional_features is not None:
-                    num_additional_features = additional_features.shape[0]
-                    if num_additional_features != num_sentences:
-                        assert num_additional_features > num_sentences
-                        additional_features = additional_features[:num_sentences]
-
-                # similar to `embedded_sentences`, add an additional dimension that corresponds to batch_size=1
-                labels = labels.unsqueeze(dim=0)
-                if confidences is not None:
-                    confidences = confidences.unsqueeze(dim=0)
-                if additional_features is not None:
-                    additional_features = additional_features.unsqueeze(dim=0)
-        else:
-            # ['CLS'] token
-            embedded_sentences = embedded_sentences[:, :, 0, :]
-            embedded_sentences = self.dropout(embedded_sentences)
-            batch_size, num_sentences, _ = embedded_sentences.size()
-            sent_mask = (mask.sum(dim=2) != 0)
-            embedded_sentences = self.self_attn(embedded_sentences, sent_mask)
+        # ['CLS'] token
+        embedded_sentences = embedded_sentences[:, :, 0, :]
+        embedded_sentences = self.dropout(embedded_sentences)
+        batch_size, num_sentences, _ = embedded_sentences.size()
+        sent_mask = (mask.sum(dim=2) != 0)
+        embedded_sentences = self.self_attn(embedded_sentences, sent_mask)
 
         if additional_features is not None:
             embedded_sentences = torch.cat((embedded_sentences, additional_features), dim=-1)
@@ -177,10 +94,8 @@ class SeqClassificationModel(Model):
         label_logits = self.time_distributed_aggregate_feedforward(embedded_sentences)
         # label_logits: batch_size, num_sentences, num_labels
 
-        if self.labels_are_scores:
-            label_probs = label_logits
-        else:
-            label_probs = torch.nn.functional.softmax(label_logits, dim=-1)
+  
+        label_probs = torch.nn.functional.softmax(label_logits, dim=-1)
 
         # Create output dictionary for the trainer
         # Compute loss and epoch metrics
@@ -198,6 +113,7 @@ class SeqClassificationModel(Model):
             # print(f"len(predicted_labels):{len(predicted_labels)}, (predicted_labels):{predicted_labels}")
 
             label_loss = 0.0
+
         if labels is not None:
             # Compute cross entropy loss
             flattened_logits = label_logits.view((batch_size * num_sentences), self.num_labels)
@@ -223,18 +139,18 @@ class SeqClassificationModel(Model):
                         crf_label_probs[i, j, label_id] = 1
                 flattened_probs = crf_label_probs.view((batch_size * num_sentences), self.num_labels)
 
-            if not self.labels_are_scores:
-                evaluation_mask = (flattened_gold != -1)
-                if flattened_probs.shape[0] == 1:
-                    self.label_accuracy(flattened_probs.float().contiguous(), flattened_gold, mask=evaluation_mask)
-                else:
-                    self.label_accuracy(flattened_probs.float().contiguous(), flattened_gold.squeeze(-1), mask=evaluation_mask)
+          
+            evaluation_mask = (flattened_gold != -1)
+            if flattened_probs.shape[0] == 1:
+                self.label_accuracy(flattened_probs.float().contiguous(), flattened_gold, mask=evaluation_mask)
+            else:
+                self.label_accuracy(flattened_probs.float().contiguous(), flattened_gold.squeeze(-1), mask=evaluation_mask)
 
-                # compute F1 per label
-                for label_index in range(self.num_labels):
-                    label_name = self.vocab.get_token_from_index(namespace='labels', index=label_index)
-                    metric = self.label_f1_metrics[label_name]
-                    metric(flattened_probs, flattened_gold, mask=evaluation_mask)
+            # compute F1 per label
+            for label_index in range(self.num_labels):
+                label_name = self.vocab.get_token_from_index(namespace='labels', index=label_index)
+                metric = self.label_f1_metrics[label_name]
+                metric(flattened_probs, flattened_gold, mask=evaluation_mask)
         
         if labels is not None:
             output_dict["loss"] = label_loss
@@ -244,17 +160,17 @@ class SeqClassificationModel(Model):
     def get_metrics(self, reset: bool = False):
         metric_dict = {}
 
-        if not self.labels_are_scores:
-            type_accuracy = self.label_accuracy.get_metric(reset)
-            metric_dict['acc'] = type_accuracy
+        
+        type_accuracy = self.label_accuracy.get_metric(reset)
+        metric_dict['acc'] = type_accuracy
 
-            average_F1 = 0.0
-            for name, metric in self.label_f1_metrics.items():
-                metric_val = metric.get_metric(reset)
-                metric_dict[name + 'F'] = metric_val["f1"]
-                average_F1 += metric_val["f1"]
+        average_F1 = 0.0
+        for name, metric in self.label_f1_metrics.items():
+            metric_val = metric.get_metric(reset)
+            metric_dict[name + 'F'] = metric_val["f1"]
+            average_F1 += metric_val["f1"]
 
-            average_F1 /= len(self.label_f1_metrics.items())
-            metric_dict['avgF'] = average_F1
+        average_F1 /= len(self.label_f1_metrics.items())
+        metric_dict['avgF'] = average_F1
 
         return metric_dict
